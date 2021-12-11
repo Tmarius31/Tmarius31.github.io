f68b991c6eb6e3b3f8a2d87a5174015260e9b223
Marius Ionut Turcanu <tmarius3101@gmail.com>
2021-12-11 19:52:01 +0100

    new section add
    portfoglio add and finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,3 +96,17 @@ modalCloses.forEach((modalClose) =>{
         })
     })
 })
+
+/*==================== navigation portfoglio ====================*/
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
